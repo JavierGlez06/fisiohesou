4de9486c8af28e2987add6904ca956f3e46ef82d
Etzahe <etzahe.b@gmail.com>
2024-11-17 23:28:31 -0600

    Base de Datos Funcionando
    HESOU Login

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
